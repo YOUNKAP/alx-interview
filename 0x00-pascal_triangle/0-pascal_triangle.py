dafb3a28e9d9118df66f52698a74395fc80fda80
YOUNKAP <nina.younkap@ccaa.pro>
2024-03-14 04:48:52 -0400

    pycod style compliant

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,15 +3,12 @@
 
 
 def pascal_triangle(n):
-    """Returns a list of lists of integers 
-    representing the Pascal’s triangle of n:
-    """
+    """Returnsthe Pascal’s triangle of n:"""
 
     if n <= 0:
         return []
 
-    
-    """ initialize an empty resulting array """
+    """initialize an empty resulting array"""
     pascal = [[] for idx in range(n)]
 
     for li in range(n):
--- 0x01-lockboxes/0-lockboxes_bon.py
+++ 0x01-lockboxes/0-lockboxes_bon.py
@@ -21,3 +21,22 @@ def canUnlockAll(boxes=[]):
         return False
 
     return True
+
+
+if __name__ == '__main__':
+    boxes = [
+                [1, 3],
+                [2],
+                [3, 0],
+                [1, 2, 3],
+            ]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes))
